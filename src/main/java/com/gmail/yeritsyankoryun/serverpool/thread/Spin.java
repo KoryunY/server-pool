30bdb6d5444108b9a192c0e7ab698a183d4988e4
KoryunY <yeritsyankoryun@gmail.com>
2021-08-01 16:23:48 +0400

    ..

--- src/main/java/com/gmail/yeritsyankoryun/serverpool/thread/Spin.java
+++ src/main/java/com/gmail/yeritsyankoryun/serverpool/thread/Spin.java
@@ -1,38 +1,15 @@
 package com.gmail.yeritsyankoryun.serverpool.thread;
 
-import com.gmail.yeritsyankoryun.serverpool.model.ServerModel;
-import com.gmail.yeritsyankoryun.serverpool.model.ApplicationModel;
-import com.gmail.yeritsyankoryun.serverpool.repository.ApplicationRepository;
-import com.gmail.yeritsyankoryun.serverpool.repository.ServerRepository;
+import static java.lang.Thread.sleep;
 
-public class Spin extends Thread {
-    private ApplicationModel applicationModel;
-    private ServerRepository serverRepository;
-    private ApplicationRepository applicationRepository;
-
-    public Spin(ApplicationModel applicationModel, ServerRepository serverRepository, ApplicationRepository applicationRepository) {
-        this.applicationModel = applicationModel;
-        this.serverRepository = serverRepository;
-        this.applicationRepository = applicationRepository;
-    }
+public class Spin implements Runnable{
 
     @Override
     public void run() {
-        ServerModel serverModel = new ServerModel();
-        serverModel.setAllocatedSize(applicationModel.getSize());
-        serverModel.setStoringDbType(applicationModel.getType());
-        serverRepository.save(serverModel);
-        applicationModel.setServerId(serverModel.getServerId());
-        serverModel.getApplicationModels().add(applicationModel);
-        synchronized (serverRepository) {
-            try {
-                sleep(7000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            serverModel.setActive(true);
-            applicationRepository.save(applicationModel);
-            serverRepository.save(serverModel);
+        try {
+            sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
